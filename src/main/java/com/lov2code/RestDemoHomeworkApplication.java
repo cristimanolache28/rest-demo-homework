4b881fc3c57be331d522e8f127f6bc9f6f74ec63
Cristian Manolache <cristianmanolache0@gmail.com>
2022-03-17 02:32:55 +0200

    add project on GitHub

---
+++ src/main/java/com/lov2code/RestDemoHomeworkApplication.java
@@ -0,0 +1,15 @@
+package com.lov2code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class RestDemoHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestDemoHomeworkApplication.class, args);
+
+    }
+
+}
